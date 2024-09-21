b360747c4319ba2f7e7c63c9b929035504785fae
Grant Schultz <gschultz1999@gmail.com>
2024-09-21 03:02:54 -0400

    9/21/24

--- JS/script.js
+++ JS/script.js
@@ -20,26 +20,3 @@ document.addEventListener("DOMContentLoaded", function() {
   document.addEventListener("scroll", function() {
     const featureItems = document.querySelectorAll(".feature-item");
 });
-
-
-  /*featureItems.forEach((item, index) => {
-        const itemRect = item.getBoundingClientRect();
-        const windowHeight = window.innerHeight;
-  
-          if (index === featureItems.length - 1) {
-              // For the last item, fade out based on its position alone
-              const opacity = Math.max(0, 1 - Math.abs(itemRect.top - windowHeight / 2) / (windowHeight / 2));
-              item.style.opacity = opacity;
-          } else {
-              const nextItemRect = featureItems[index + 1].getBoundingClientRect();
-  
-              if (nextItemRect.top < windowHeight && nextItemRect.bottom > 0) {
-                  // Fade out the current item as the next item comes into view
-                  const opacity = Math.max(0, (nextItemRect.top - windowHeight / 2) / (windowHeight / 2));
-                  item.style.opacity = 1 - opacity;
-              } else {
-                  // Ensure the item is fully opaque if the next item is not in view
-                  item.style.opacity = 1;
-              }
-          }
-      /*});*/
